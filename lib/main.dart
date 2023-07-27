c7fd565d4754b03a92dc108e44078c3187ccf64b
asfand naveed <m.asfandnaveed@gmail.com>
2023-07-27 12:21:01 +0500

    27-07-2023

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 
 void main() {
-  runApp(const Sample());
+  runApp(const Home());
 }
 
 class Home extends StatelessWidget {
@@ -15,70 +15,282 @@ class Home extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                    'Corvit Mobile App',
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xff5a9fd2),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                      'Corvit Mobile App',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xff5a9fd2),
+                    ),
                   ),
-                ),
 
-                Container(
-                  width: 300,
-                  height: 400,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(350),
-                        topRight: Radius.circular(15),
+                  Stack(
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 400,
+                        padding: EdgeInsets.all(10),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(350),
+                              topRight: Radius.circular(15),
+                            ),
+                          ),
+                          elevation: 10,
+                          child: Column(
+
+                            children: [
+                              Container(
+
+                                padding: EdgeInsets.all(15),
+                                  child: Image.asset(
+                                    'assets/images/facial_product.png',
+                                    height: 200,
+                                  ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                      'Perfume',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 23,
+                                    ),
+                                  ),
+                                  Text(
+                                    '\$ 560',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              ElevatedButton(
+                                  onPressed: (){},
+                                  child: Text(
+                                    'Add to cart'
+                                  )
+                              ),
+                            ],
+                          ),
+
+                        ),
                       ),
-                    ),
-                    elevation: 10,
-                    child: Column(
+                      Positioned(
+                        left: 260,
+                        top: 35,
+                        child: Container(
+                          width: 40,
+                          height: 320,
+                          decoration: BoxDecoration(
 
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(15),
-                            child: Image.asset('assets/images/perfume.png'),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Text(
-                                'Perfume',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 23,
-                              ),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(300),
+                              topRight: Radius.circular(20),
                             ),
-                            Text(
-                              '\$ 560',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 18,
+                            color: Colors.blueAccent,
+                          ),
+                          child: RotatedBox(
+                            quarterTurns: 1,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 20),
+                                child: Text('Facial Cleanser',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ),
-                            ),
-                          ],
+                          ),
                         ),
-                        ElevatedButton(
-                            onPressed: (){},
-                            child: Text(
-                              'Add to cart'
-                            )
-                        ),
-                      ],
-                    ),
-
+                      ),
+                    ],
                   ),
-                ),
+                  Stack(
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 400,
+                        padding: EdgeInsets.all(10),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(350),
+                              topRight: Radius.circular(15),
+                            ),
+                          ),
+                          elevation: 10,
+                          child: Column(
+
+                            children: [
+                              Container(
+
+                                padding: EdgeInsets.all(15),
+                                child: Image.asset(
+                                  'assets/images/shoes.png',
+                                  height: 200,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    'Perfume',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 23,
+                                    ),
+                                  ),
+                                  Text(
+                                    '\$ 560',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              ElevatedButton(
+                                  onPressed: (){},
+                                  child: Text(
+                                      'Add to cart'
+                                  )
+                              ),
+                            ],
+                          ),
+
+                        ),
+                      ),
+                      Positioned(
+                        left: 260,
+                        top: 35,
+                        child: Container(
+                          width: 40,
+                          height: 320,
+                          decoration: BoxDecoration(
+
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(300),
+                              topRight: Radius.circular(20),
+                            ),
+                            color: Colors.blueAccent,
+                          ),
+                          child: RotatedBox(
+                            quarterTurns: 1,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 20),
+                              child: Text('Leather Shoes',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 400,
+                        padding: EdgeInsets.all(10),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(350),
+                              topRight: Radius.circular(15),
+                            ),
+                          ),
+                          elevation: 10,
+                          child: Column(
+
+                            children: [
+                              Container(
+
+                                padding: EdgeInsets.all(15),
+                                child: Image.asset(
+                                  'assets/images/sunglass.png',
+                                  height: 200,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    'Perfume',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 23,
+                                    ),
+                                  ),
+                                  Text(
+                                    '\$ 560',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              ElevatedButton(
+                                  onPressed: (){},
+                                  child: Text(
+                                      'Add to cart'
+                                  )
+                              ),
+                            ],
+                          ),
+
+                        ),
+                      ),
+                      Positioned(
+                        left: 260,
+                        top: 35,
+                        child: Container(
+                          width: 40,
+                          height: 320,
+                          decoration: BoxDecoration(
+
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(300),
+                              topRight: Radius.circular(20),
+                            ),
+                            color: Colors.blueAccent,
+                          ),
+                          child: RotatedBox(
+                            quarterTurns: 1,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 20),
+                              child: Text('Gucci Sun Glasses',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
 
 
 
-              ],
+                ],
+              ),
             ),
           ),
         ),
